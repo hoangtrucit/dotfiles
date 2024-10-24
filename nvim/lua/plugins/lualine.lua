2732fac7b5b1fcb6aa36221cb36a2dd672e284b2
truc.tran <hoangtrucit@gmailcom>
2024-10-25 01:50:52 +0700

    feat: custom lualine, remove dbui

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -6,7 +6,36 @@ return {
 	event = { "BufReadPost", "BufNewFile", "VeryLazy" },
 	config = function()
 		require("lualine").setup({
-			options = { theme = "ayu_dark" },
+			options = {
+				theme = "ayu_dark",
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = {
+					"encoding",
+					"filetype",
+					{
+						"fileformat",
+						symbols = {
+							unix = "", -- e712
+							dos = "", -- e70f
+							mac = "", -- e711
+						},
+					},
+				},
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
 		})
 	end,
 }
